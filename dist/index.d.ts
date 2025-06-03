78627a11452b3c21960b484e41042b5c12aba0ce
Artavazd Barseghyan <artavazd1@gmail.com>
2025-06-04 01:02:22 +0200

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import { CorrelatedRequestDTO, CorrelatedResponseDTO } from 'kafka-pkg';
 import { DidSendEmailDTO, SendEmailDTO } from './src/types/email.dto';
 import { EmailKafkaTopic } from './src/common/constants';
+import { validateSendEmailDTO } from './src/common/email-delivery';
 export declare function sendEmail(dto: CorrelatedRequestDTO<SendEmailDTO>, callback?: (response: CorrelatedResponseDTO<DidSendEmailDTO>) => void): void;
-export { SendEmailDTO, DidSendEmailDTO, EmailKafkaTopic, CorrelatedRequestDTO, CorrelatedResponseDTO, };
+export { SendEmailDTO, DidSendEmailDTO, EmailKafkaTopic, CorrelatedRequestDTO, CorrelatedResponseDTO, validateSendEmailDTO };
--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmailKafkaTopic = void 0;
+exports.validateSendEmailDTO = exports.EmailKafkaTopic = void 0;
 exports.sendEmail = sendEmail;
 const smtp_service_1 = __importDefault(require("./src/services/smtp-service"));
 const constants_1 = require("./src/common/constants");
 Object.defineProperty(exports, "EmailKafkaTopic", { enumerable: true, get: function () { return constants_1.EmailKafkaTopic; } });
+const email_delivery_1 = require("./src/common/email-delivery");
+Object.defineProperty(exports, "validateSendEmailDTO", { enumerable: true, get: function () { return email_delivery_1.validateSendEmailDTO; } });
 function sendEmail(dto, callback) {
     smtp_service_1.default.sendEmail(dto, callback);
 }
