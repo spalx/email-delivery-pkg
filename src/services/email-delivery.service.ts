75c34f5ea113a5142d354d918d4ca4f1086348bd
Artavazd Barseghyan <artavazd1@gmail.com>
2025-09-12 14:23:12 +0200

    set default transport

--- src/services/email-delivery.service.ts
+++ src/services/email-delivery.service.ts
@@ -1,11 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
-import { CorrelatedMessage, TransportAwareService, transportService } from 'transport-pkg';
+import { CorrelatedMessage, TransportAwareService, TransportAdapterName, transportService } from 'transport-pkg';
 import { IAppPkg, AppRunPriority } from 'app-life-cycle-pkg';
 
 import { SendEmailDTO } from '../types/email-delivery.dto';
 import { EmailDeliveryAction } from '../common/constants';
 
 class EmailDeliveryService extends TransportAwareService implements IAppPkg {
+  async init(): Promise<void> {
+    //TODO: use service-discovery here
+    this.useTransport(TransportAdapterName.HTTP, { host: 'email-delivery', port: 3040 });
+  }
+
   getPriority(): number {
     return AppRunPriority.Highest;
   }
