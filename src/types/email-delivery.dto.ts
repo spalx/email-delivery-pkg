7d5bfc554aaa0241097488c0a0c4d02b84d8150a
Artavazd Barseghyan <artavazd1@gmail.com>
2025-10-06 22:03:33 +0200

    update packages

--- src/types/email-delivery.dto.ts
+++ src/types/email-delivery.dto.ts
@@ -20,12 +20,12 @@ export interface DidSendEmailDTO {
 
 export const SendEmailDTOSchema = z.object({
   from: z.string('from must be a string').min(1, 'from cannot be empty'),
-  to: z.array(z.string().min(1, 'recipient email cannot be empty'), 'to must be an array').min(1, 'to must not be empty'),
+  to: z.array(z.string('each to must be a string').min(1, 'each to cannot be empty'), 'to must be an array').min(1, 'to must not be empty'),
   subject: z.string('subject must be a string').min(1, 'subject cannot be empty'),
   body: z.string('body must be a string').min(1, 'body cannot be empty'),
   attachments: z.record(z.string(), z.any()).optional(),
   inline: z.record(z.string(), z.any()).optional(),
-  bcc: z.array(z.string().min(1, 'bcc email cannot be empty')).optional(),
-  cc: z.array(z.string().min(1, 'cc email cannot be empty')).optional(),
-  replyTo: z.string().optional(),
+  bcc: z.array(z.string('each bcc must be a string').min(1, 'each bcc cannot be empty'), 'bcc must be an array').optional(),
+  cc: z.array(z.string('each cc must be a string').min(1, 'each cc cannot be empty'), 'cc must be an array').optional(),
+  replyTo: z.string('replyTo must be a string').optional(),
 });
